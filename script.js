61bf2353afc470204160b6762bf153148a093018
Ewenne <221577967+ew-enne@users.noreply.github.com>
2025-10-03 09:41:03 +0200

    Create square grid with JS (and remove HTML)
    This replaces the pure HTML version with JS.

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-let numberOfSquares = 16;
+let container = document.querySelector('.container');
 
-/*
-for (let i = 0; i < numberOfSquares, i++) {
+let squaresPerSide = 4;
 
-}*/
+// create divs for the grid
+for (let i = 0; i < squaresPerSide; i++) {
+    let row = document.createElement('div');
+    container.appendChild(row);
+    row.classList.add("square-row");
+
+    for (let j = 0; j < squaresPerSide; j++) {
+        let column = document.createElement('div');
+        row.appendChild(column);
+        column.classList.add("square-column");
+    }
+}
